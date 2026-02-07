242127c6179d57ffbb13d487d4c08a816880b6b0
defyded <akou096@gmail.com>
2026-02-07 14:08:57 +0300

    idk why JwtTokenService dont commit

--- IntegrationTests/Service test/JwtTokenServiceTest.cs
+++ IntegrationTests/Service test/JwtTokenServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using FluentAssertions;
+using Messenger.Domain.Entities;
+using Messenger.Services;
+using Messenger.Settings;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,32 @@ using System.Threading.Tasks;
 
 namespace IntegrationTests.Service_test
 {
-    class JwtTokenService
+    public class JwtTokenServiceTest
     {
+        public static JwtTokenService CreateSut()
+        {
+            var opt = Options.Create(new JwtOptions
+            {
+                Issuer = "test-issuer",
+                Audience = "test-audince",
+                SigningKey = "DtA4wXCgHmy9wi2oHNYJK0t5tyFwtHoUcLVB71S5WpV", 
+                AccesTokenMinutes = 60
+            });
+            return new JwtTokenService(opt);
+        }
+        [Fact]
+        public void CreateAccesToken_returns_valid_token()
+        {
+            var svc = CreateSut();
+            var user = new User
+            {
+                Username = "alice",
+                Email = "any@gmail.com",
+                PasswordHash = "qwerty12345"
+            };
+            var (token, expires) = svc.CreateAccesToken(user);
+            token.Should().NotBeNullOrWhiteSpace();
+            expires.Should().BeAfter(DateTime.UtcNow);
+        }
     }
 }
